24170d2dd5f5eaaeec550e397ac82034d87fb92d
abdullahmohamedali <nehadebeed@gmail.com>
2022-07-21 14:59:20 +0200

    frist commit

---
+++ python.py
@@ -0,0 +1,38 @@
+#--------------------- 
+#--strring formate----
+#---------------------
+
+name="abdullah"
+age=9
+rank=10
+
+print("my name is {} ".format(name))
+
+print("my name is {:s} and my age is {:d} and my rank is {:f}".format(name,age,rank))
+
+n = "abdullah"
+l ="python"
+y =10
+
+print(" my name is {} i am a {} devolaper with {:d} exp".format(n,l,y))
+
+mynumber= 10
+
+print("my number is {:d}".format (mynumber))
+
+print("my number is {:.2f}".format (mynumber))
+
+mylongstring = ("python error hello world")
+print("mesege is {}".format(mylongstring))
+print("mesege is {:.6s}".format(mylongstring))
+
+mymony = ("9087908709809868793333333333333333342")
+
+print("mymony in the bank is {:d}".format(mymony))
+
+print("mymony in the bank is{:_d}".format(mymony))
+print("mymony in the bank is{:,d}".format(mymony))
+
+
+
+
